de40e96ae79f8dda32151f9e3a228ba89300a0d8
Casey Perkins <uz1983rm@mnstate.edu>
2022-03-08 11:57:11 -0600

    data is in!

--- Casey's File.R
+++ Casey's File.R
@@ -4,7 +4,9 @@ file_url<-"https://esajournals.onlinelibrary.wiley.com/action/downloadSupplement
 file_name <- basename(file_url)
 
 if (!dir.exists("bird_data")) dir.create("bird_data")
+
 #if (!file.exists(file_name))
 download.file(file_url, destfile = "bird_data/bird_data.zip", mode = "wb")
+unzip("bird_data/bird_data.zip", overwrite = TRUE, exdir = "bird_data")
 
-read_excel("bird_data/bird_data.xlsx")
+read_csv("bird_data/ATLANTIC_BIRD_TRAITS_completed_2018_11_d05.csv")
