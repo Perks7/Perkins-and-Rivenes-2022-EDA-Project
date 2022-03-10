a0e828e899fa509d505a340fc96385b60f4065f8
Bryn Rivenes <su6143fp@mnstate.edu>
2022-03-10 10:54:22 -0600

    beginning stuff - bryn

---
+++ Bryn's File.R
@@ -0,0 +1,11 @@
+#Renaming Info
+
+
+
+#New Variable
+length_width <- (select(bird_data, body_length_mm, body_mass_g))
+
+length_width
+
+mutate(length_width, mass_per_length = )
+
--- Project File.R
+++ Project File.R
@@ -7,6 +7,8 @@ if (!dir.exists("bird_data")) dir.create("bird_data")
 
 #if (!file.exists(file_name))
 download.file(file_url, destfile = "bird_data/bird_data.zip", mode = "wb")
+
 unzip("bird_data/bird_data.zip", overwrite = TRUE, exdir = "bird_data")
 
 read_csv("bird_data/ATLANTIC_BIRD_TRAITS_completed_2018_11_d05.csv")
+
