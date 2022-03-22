b4e0d22d11c72b07e09ecf4dffd1a6027bfe819e
Bryn Rivenes <su6143fp@mnstate.edu>
2022-03-22 10:55:04 -0500

    test

--- Bryn's File.R
+++ Bryn's File.R
@@ -3,5 +3,6 @@ length_width <- select(bird_data, body_length_mm, body_mass_g)
 
 length_width
 
-mutate(length_width, length_mass_ratio = body_length_mm / body_mass_g)
-
+mutate(length_width, length_mass_ratio = body_length_mm / body_mass
+      
+#test
