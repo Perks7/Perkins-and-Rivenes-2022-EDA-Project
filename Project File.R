c4f7b94b9f230f72f32ca1b311c20e64b9d7b3df
Casey Perkins <uz1983rm@mnstate.edu>
2022-03-31 11:04:45 -0500

    t-test is done!

--- Project File.R
+++ Project File.R
@@ -1,5 +1,14 @@
+
+
+# load packages -----------------------------------------------------------
+
+
 library(tidyverse)
 library(ggrepel)
+
+
+# read data ---------------------------------------------------------------
+
 
 
 file_url<-"https://esajournals.onlinelibrary.wiley.com/action/downloadSupplement?doi=10.1002%2Fecy.2647&file=ecy2647-sup-0001-DataS1.zip"
@@ -13,6 +22,10 @@ unzip("bird_data/bird_data.zip", overwrite = TRUE, exdir = "bird_data")
 
 #reading and renaming data
 bird_data<-read_csv("bird_data/ATLANTIC_BIRD_TRAITS_completed_2018_11_d05.csv")
+
+
+# clean up data -----------------------------------------------------------
+
 
 #renaming variables 
 bird_data <- rename(bird_data, 
@@ -28,7 +41,10 @@ length_vs_mass <- select(bird_data, body_length_mm, body_mass_g)
 length_vs_mass <- mutate(bird_data, 
                          length_mass_ratio = body_length_mm / body_mass_g)
 
-#ggplot Length Mass Ratio vs Altitude 
+
+# plots -------------------------------------------------------------------
+
+#ggplot Length Mass Ratio vs Altitude
 ggplot(data = length_vs_mass) +
   geom_point(mapping = aes(
     x = length_mass_ratio, 
@@ -82,6 +98,9 @@ ggplot(data = length_vs_mass) +
     x = length_mass_ratio, 
     y = altitude))+
   facet_wrap(~Order)
+
+
+# length vs mass ----------------------------------------------------------
 
 #log of length_mass_ratio to correct right skew 
 length_vs_mass <-mutate(length_vs_mass, ln_lmr = log(length_mass_ratio))
@@ -211,16 +230,50 @@ length_vs_mass %>%
   geom_point() +
   geom_smooth(method = "lm")
 
+
 #table of mean_ln_lmr and mean_altitude passeriformes
-passeriformes_means<-length_vs_mass %>% 
+passeriformes_means<-
+  length_vs_mass %>% 
+  filter(!is.na(altitude), !is.na(ln_lmr)) %>% 
   group_by(Order, Family, Genus, Species) %>% 
-  summarize(ln_lmr = mean(ln_lmr),
-            altitude = mean(altitude)) %>%
-  mutate(mean_ln_lmr = mean(ln_lmr),
-            mean_altitude = mean(altitude))
+  summarize(
+    ln_lmr = mean(ln_lmr),
+    altitude = mean(altitude)
+  ) %>% 
+  print()
+
+
+# t-tests -----------------------------------------------------------------
 
 #filter smaller and larger means
-passeriformes_means %>%
+
+mean_hilo <-
+  passeriformes_means %>% 
+  mutate(hilo = ifelse(altitude>1000, "Above 1000 m", "Below 1000 m")) %>% 
+  group_by(hilo) %>% 
+  summarize(
+    mean = mean(ln_lmr),
+    # sd = sd(ln_lmr, na.rm = TRUE),
+    # sem = sd(ln_lmr)/sqrt(n()),
+    # upper = ln_lmr + 2*sem,
+    # lower = ln_lmr - 2*sem
+  )
+
+passeriformes_means %>% 
+  mutate(hilo = ifelse(altitude>1000, "Above 1000 m", "Below 1000 m")) %>%
+  ggplot(aes(x = ln_lmr)) +
+  geom_histogram() +
+  geom_vline(aes(xintercept = mean), data = mean_hilo,
+             linetype = "dashed", size = 2, color = "red") +
+  facet_wrap(~ hilo, ncol = 1, scales = "free_y")
+
+passeriformes_means %>% 
+  mutate(hilo = ifelse(altitude>1000, "Above 1000 m", "Below 1000 m")) %>% 
+  t.test(ln_lmr ~ hilo, data = .)
+
+
+
+
   filter(mean_altitude>1000,) %>%
   print()
 passeriformes_means %>%
